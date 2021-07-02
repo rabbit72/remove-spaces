d534f557c0e2f078e61386e6fb596d558de3dff1
Daniil Shadrin <rabbit72rus@gmail.com>
2021-07-02 17:12:32 +0300

    #1 fix rust implementation

--- rust/main.rs
+++ rust/main.rs
@@ -24,38 +24,15 @@ fn main() {
 
 
 pub fn remove_extra_spaces(target: &mut String) {
-    const SPACE: char = ' ';
-    let mut from_index = 0; // copy from this index
-    let mut to_index = 0;  // copy to this index
-    let mut is_previous_space: bool = false;  // set true when a single space is written
+    let mut is_previous_space: bool = true;  // set true when a single space is written
 
-    let original_length = target.len();
+    // step 1: processing all chars
+    target.retain(|ch| {
+        let retain: bool = !ch.is_whitespace() || !is_previous_space;
+        is_previous_space = ch.is_whitespace();
+        retain
+    });
 
-    // step 1: skipping spaces at the begining
-    while from_index < original_length && target.chars().nth(from_index).unwrap() == SPACE {
-        from_index += 1;
-    }
- 
-    // step 2: processing all chars after possible spaces
-    while from_index < original_length {
-        if target.chars().nth(from_index).unwrap() != SPACE {  // copies 'from_index' to 'to_index'
-            let ch = target.chars().nth(from_index).unwrap().to_string();
-            target.replace_range(to_index..to_index + 1, &ch);
-            to_index += 1;
-            is_previous_space = false;
-        } else {
-            if !is_previous_space {  //puts one space if the previous is not a space
-                target.replace_range(to_index..to_index + 1, &SPACE.to_string());
-                to_index += 1;
-                is_previous_space = true;  //stops appearing more than 1 space
-            }
-        }
-        from_index += 1;
-    }
-    // step 3: cutting the string inplace
-    if to_index > 1 && is_previous_space {
-        target.truncate(to_index - 1);
-    } else {
-        target.truncate(to_index);
-    }
+    // step 2: cutting the possible last whitespace
+    target.truncate(target.trim_end().len());
 }
